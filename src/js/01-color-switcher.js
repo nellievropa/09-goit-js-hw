842cac62f42ed1828286a9a8697e71e0f20ad746
nellievropa <nellievropa23@gmail.com>
2023-11-10 13:20:14 +0200

    Homework

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,42 @@
 
+import throttle from "lodash.throttle";
+
+const CHANGE_COLOR = 500;
+
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]')
+
+let TIMER_Id = null;
+// let startBtn.disabled = false;
+
+startBtn.addEventListener('click', throttle(onStart, 500));
+stopBtn.addEventListener('click', onStop);
+
+
+function onStart(evt) {
+  if (!evt) {
+       return;
+  }
+        console.log('Змінюємо колір!');
+        TIMER_Id = setInterval(changeColors,CHANGE_COLOR);
+        startBtn.disabled = true;
+ }
+    
+
+function onStop() {
+   
+    console.log('Зупиняємо інтервал!');
+    clearInterval(TIMER_Id);  
+    startBtn.disabled = false;
+      
+
+}
+
+function changeColors() {
+    console.log('Змінюємо колір!');
+    document.body.style.background = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
